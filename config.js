1cfecd2475a32d31b5b93524586827a290b4c1aa
TovaryProject <112696901+vladosssssssss@users.noreply.github.com>
2026-09-03 01:21:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   BACKEND_URL: "https://script.google.com/macros/s/AKfycbwyn28J6ZZtFOHPUx92aTY4pg11dBh2ScitzuY1dDHkDlBHFYIp7ZqvhCdee5bqnJGd/exec",
-  UAH_RATE: 100
+  UAH_RATE: 52
 };
